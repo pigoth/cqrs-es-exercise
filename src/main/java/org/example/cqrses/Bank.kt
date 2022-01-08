4dd55c1ad0927965b33d24b7cb34d8b163f82e8f
Thomas Ferrari <thomas.ferrari@cherrychain.it>
2022-01-08 14:15:29 +0100

    initial commit

---
+++ src/main/java/org/example/cqrses/Bank.kt
@@ -0,0 +1,7 @@
+package org.example.cqrses
+
+class Bank {
+
+
+
+}
---
+++ src/test/java/org/example/cqrses/BankTest.kt
@@ -0,0 +1,12 @@
+package org.example.cqrses
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class BankTest {
+
+  @Test
+  internal fun dummy_test() {
+   assertThat(1).isEqualTo(1)
+  }
+}

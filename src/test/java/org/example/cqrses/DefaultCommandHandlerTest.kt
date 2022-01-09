5429ba29637907c41e03d312463cb6f08d3f6e61
Thomas Ferrari <thomas.ferrari@cherrychain.it>
2022-01-09 10:50:05 +0100

    test refactor

--- src/test/java/org/example/cqrses/DefaultCommandHandlerTest.kt
+++ src/test/java/org/example/cqrses/DefaultCommandHandlerTest.kt
@@ -4,20 +4,27 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.example.cqrses.domain.*
+import org.example.cqrses.logic.CommandHandler
 import org.example.cqrses.logic.DefaultCommandHandler
 import org.example.cqrses.port.CustomerRepository
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.UUID.randomUUID
 
 internal class DefaultCommandHandlerTest {
 
+  private lateinit var commandHandler: CommandHandler
   private val customerRepository: CustomerRepository = mockk()
+
+  @BeforeEach
+  internal fun setUp() {
+    commandHandler = DefaultCommandHandler(customerRepository)
+
+    every { customerRepository.put(any()) } returns Unit
+  }
 
   @Test
   internal fun should_acquire_a_customer() {
-    every { customerRepository.put(any()) } returns Unit
-
-    val commandHandler = DefaultCommandHandler(customerRepository)
     commandHandler.handle(
       AcquireCustomer(
         id = randomUUID(),
@@ -34,11 +41,9 @@ internal class DefaultCommandHandlerTest {
   @Test
   internal fun should_modify_customer_personal_data() {
     val customerId = randomUUID()
-    every { customerRepository.put(any()) } returns Unit
     every { customerRepository.load(customerId) } returns Customer()
       .hydrate(listOf<Event>(CustomerAcquired(customerId, "Gino", "Paoli", "GinoFiscalCode", "GinoAddress")))
 
-    val commandHandler = DefaultCommandHandler(customerRepository)
     commandHandler.handle(ModifyCustomerPersonalData(customerId, "new address"))
 
     verify {

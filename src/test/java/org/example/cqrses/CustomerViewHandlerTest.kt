b8bf2619a0f5cd17037515453cb1cc8eaa632ae7
Thomas Ferrari <thomas.ferrari@cherrychain.it>
2022-01-09 11:50:58 +0100

    refactor: test introduce setup

--- src/test/java/org/example/cqrses/CustomerViewHandlerTest.kt
+++ src/test/java/org/example/cqrses/CustomerViewHandlerTest.kt
@@ -10,6 +10,7 @@ import org.example.cqrses.domain.Event
 import org.example.cqrses.logic.CustomerViewHandler
 import org.example.cqrses.port.CustomerView
 import org.example.cqrses.port.repository.CustomerViews
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 import java.time.LocalDateTime.now
@@ -18,26 +19,30 @@ import java.util.*
 internal class CustomerViewHandlerTest {
 
   private val customerViews: CustomerViews = mockk()
+  private lateinit var eventBus: EventBus
+
+  @BeforeEach
+  internal fun setUp() {
+    eventBus = EventBus()
+      .apply { register(CustomerViewHandler(customerViews)) }
+
+    every { customerViews.put(any()) } returns Unit
+  }
 
   @Test
   internal fun should_add_new_costumer_when_acquired() {
-    every { customerViews.put(any()) } returns Unit
-
     val id = UUID.randomUUID()
-    EventBus()
-      .apply { register(CustomerViewHandler(customerViews)) }
-      .apply {
-        post(
-          CustomerAcquired(
-            id = id,
-            name = "Gino",
-            surname = "Paoli",
-            fiscalCode = "GinoFiscalCode",
-            address = "GinoAddress",
-            at = now()
-          )
-        )
-      }
+
+    eventBus.post(
+      CustomerAcquired(
+        id = id,
+        name = "Gino",
+        surname = "Paoli",
+        fiscalCode = "GinoFiscalCode",
+        address = "GinoAddress",
+        at = now()
+      )
+    )
 
     verify { customerViews.put(CustomerView(id, "Gino", "Paoli", "GinoAddress")) }
   }
@@ -46,13 +51,8 @@ internal class CustomerViewHandlerTest {
   internal fun should_update_personal_data_when_customer_modified_its() {
     val id = UUID.randomUUID()
     every { customerViews.get(id) } returns CustomerView(id, "Gino", "Paoli", "old address")
-    every { customerViews.put(any()) } returns Unit
 
-    EventBus()
-      .apply { register(CustomerViewHandler(customerViews)) }
-      .apply {
-        post(CustomerPersonalDataModified(id, "new address", now()))
-      }
+    eventBus.post(CustomerPersonalDataModified(id, "new address", now()))
 
     verify { customerViews.put(CustomerView(id, "Gino", "Paoli", "new address")) }
   }

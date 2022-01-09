90b0bad1905f8d2bf3cf65671a05c8ce28f50514
Thomas Ferrari <thomas.ferrari@cherrychain.it>
2022-01-09 14:23:54 +0100

    resolve warning: rename parameter

--- src/main/java/org/example/cqrses/port/repository/CustomerViews.kt
+++ src/main/java/org/example/cqrses/port/repository/CustomerViews.kt
@@ -4,7 +4,7 @@ import org.example.cqrses.port.CustomerView
 import java.util.*
 
 interface CustomerViews {
-  fun put(customerView: CustomerView)
+  fun put(view: CustomerView)
   fun all(): List<CustomerView>
   fun get(id: UUID) : CustomerView
 
